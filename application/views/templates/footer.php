c47cda5bf25baab4c951c37e0568bc70f62c4616
daffakurnia11 <daffakurniaf11@gmail.com>
2021-05-23 18:36:58 +0700

    Detailing Footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,26 +2,81 @@
   <div class="footer d-flex flex-column justify-content-center">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-lg-5 text-center">
-          <h2>Kontak kami untuk informasi lebih lanjut</h2>
-          <input type="text" class="form-control my-4" placeholder="Nama">
-          <input type="text" class="form-control my-4" placeholder="Email">
-          <div class="contact d-flex justify-content-between">
-            <a href="https://www.instagram.com/breakpoint.idn/" target="_blank">
-              <i class="contact-icon flaticon-instagram"></i>
-            </a>
-            <a href="https://wa.me/6285156317473" target="_blank">
-              <i class="contact-icon flaticon-whatsapp"></i>
-            </a>
-            <a href="" target="_blank">
-              <i class="contact-icon flaticon-linkedin"></i>
-            </a>
-            <a href="mailto:breakpoint.idn@gmail.com" target="_blank">
-              <i class="contact-icon flaticon-gmail"></i>
-            </a>
-            <a href="http://line.me/ti/p/~daffakurnia11" target="_blank">
-              <i class="contact-icon flaticon-line"></i>
-            </a>
+        <div class="col-sm-6 col-md-4">
+          <h2>Breakpoint</h2>
+          <div class="d-flex align-items-center">
+            <i class="flaticon-pin"></i>
+            <span class="ps-3">Gedung Graha Pena Lt 15, jalan A. Yani,<br>Surabaya, Jawa Timur</span>
+          </div>
+          <a href="https://wa.me/6285790290209" target="_blank" class="footer-link">
+            <p class="my-3">
+              <i class="flaticon-phone-call"></i>
+              <span class="ps-3">+62 857 9029 0209 (Kholid)</span>
+            </p>
+          </a>
+          <a href="https://wa.me/6285156317473" target="_blank" class="footer-link">
+            <p class="my-3">
+              <i class="flaticon-phone-call"></i>
+              <span class="ps-3">+62 851 5631 7473 (Daffa)</span>
+            </p>
+          </a>
+          <a href="https://www.instagram.com/breakpoint.idn/" target="_blank" class="footer-link">
+            <p class="my-3">
+              <i class="flaticon-instagram"></i>
+              <span class="ps-3">@breakpoint.idn</span>
+            </p>
+          </a>
+          <a href="mailto:breakpoint.idn@gmail.com" class="footer-link">
+            <p class="my-3">
+              <i class="flaticon-gmail"></i>
+              <span class="ps-3">breakpoint.idn@gmail.com</span>
+            </p>
+          </a>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <h2>Jam Buka</h2>
+          <p class="py-2">Senin - Jumat (07.00 WIB - 17.00 WIB)</p>
+          <p class="py-2">Sabtu (09.00 WIB - 15.00 WIB)</p>
+          <p class="py-2">Minggu (LIBUR)</p>
+          <p class="py-2">Konsultasi 24/7 Jam</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+          <h2 class="text-center text-md-start">Paket dan Harga</h2>
+          <div class="row">
+            <div class="col-sm-6 col-md-12">
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Custom Website</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Landing Page</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Landing Page+</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Small Business</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Medium Business</p>
+              </a>
+            </div>
+            <div class="col-sm-6 col-md-12">
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Professional Business</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Webinar Website</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Event Website</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">Mini Classroom</p>
+              </a>
+              <a href="<?= base_url('package'); ?>" class="footer-link">
+                <p class="py-1">E-commerce Website</p>
+              </a>
+            </div>
           </div>
         </div>
       </div>
