810cf5eb62432b65d5f714ebb6e9f1cfbfd50d3c
daffakurnia11 <daffakurniaf11@gmail.com>
2021-06-01 23:38:11 +0700

    Fixing Unnecessary Files

--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -31,14 +31,6 @@
 <script src="<?= base_url('assets/'); ?>dist/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="<?= base_url('assets/'); ?>dist/datatables-responsive/js/dataTables.responsive.min.js"></script>
 <script src="<?= base_url('assets/'); ?>dist/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/jszip/jszip.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/pdfmake/pdfmake.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/pdfmake/vfs_fonts.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="<?= base_url('assets/'); ?>dist/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/'); ?>js/adminlte.min.js"></script>
 <script>
